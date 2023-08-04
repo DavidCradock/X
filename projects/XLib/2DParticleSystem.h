acdbf27de85734c7e30672276c097d67a2eb2f21
David Cradock <djpcradock@gmail.com>
2023-08-04 06:01:51 +0100

    Worked upon particle system a bit

--- projects/XLib/2DParticleSystem.h
+++ projects/XLib/2DParticleSystem.h
@@ -7,19 +7,36 @@
 namespace X
 {
 	// A 2D particle system class used with the SC2DRenderer class.
-	// A particle system spawns individual particles which each have their own lifespan and throughout a particle's life,
-	// has various values which interpolate between values over the course of their life.
-	// These values include position, rotation, radius, colour(RGBA) and various images stored inside a CResourceTextureAtlas.
+	// 
+	// A particle system consists of multiple objects including emitters, effectors, particle types and the particles themselves.
+	// We use just one particle system which holds all particles. This way, all particles being spawned will be able to be sorted correctly, otherwise
+	// if we have multiple systems, each one, when being rendered near each other would have no way of sorting their own particles by depth of the other particle systems.
+	// 
+	// Emitters...
+	// 
+	// 
+	// Effectors...
+	// 
+	// 
+	// 
+	// Particles...
+	// A particle system spawns individual particles which each have their own lifespan.
+	// A particle has multiple stages set throughout it's life span.
+	// Each stage has some values which include, rotation, radius, colour(RGBA) and various images stored inside a CResourceTextureAtlas.
 	// A particle's life span is represented by a float from 0.0f (birth) through to 1.0f (death)
-	// A particle can have multiple life stages set between their life, set between 0 and 1
+	// As a particle ages, these stage values are interpolated between to create their current appearance.
 	// For example, a particle could have 2 life stages, one at 0 (birth) with bright colours and high alpha and another at 1 (death) with dark colours and zero alpha, so it
 	// fades out just before it dies.
 	//
-	// A particle system contains many quads rendered to the screen using instancing for reduced draw calls for great performance.
-	// A particle system uses a CResourceTextureAtlas to hold the various images it uses when rendering each individual particle to reduce texture bindings.
+	// Particle types...
+	// 
+	// 
+	// 
 	//
 	// Implementation details
-	// To deal with the multiple textures, a texture atlas is used and in the fragment shader, different texture coordinates are used to lookup the correct texture image.
+	// To deal with the multiple textures, a texture atlas is used and in the shaders, different texture coordinates are used
+	// to lookup the correct texture image this reduces texture bindings.
+	// A particle system contains many quads rendered to the screen using instancing for reduced draw calls for great performance.
 	class C2DParticleSystem
 	{
 	public:
